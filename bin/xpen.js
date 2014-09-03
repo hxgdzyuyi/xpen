0b080feace6dbd619eadefb9cfd979eea261d5ad
hxgdzyuyi <hxgdzyuyi@gmail.com>
2014-09-03 11:55:43 +0800

    Fix xpen serve 端口可配置

--- bin/xpen.js
+++ bin/xpen.js
@@ -5,30 +5,28 @@ var cli = require('../lib/cli')
 var args = process.argv.slice(2)
   , clc = require('cli-color')
 
-while (args.length) {
-  arg = args.shift()
-  switch(arg) {
-    case 'i':
-    case 'init':
-      cli.init(args.shift())
-      break;
+arg = args.shift()
+switch(arg) {
+  case 'i':
+  case 'init':
+    cli.init(args.shift())
+    break;
 
-    case 'f':
-    case 'fetch':
-      cli.fetch(args.shift())
-      break;
+  case 'f':
+  case 'fetch':
+    cli.fetch(args.shift())
+    break;
 
-    case 's':
-    case 'serve':
-      cli.serve()
-      break;
+  case 's':
+  case 'serve':
+    cli.serve(args)
+    break;
 
-    case 'u':
-    case 'uplaod':
-      cli.upload(args.shift())
-      break;
+  case 'u':
+  case 'uplaod':
+    cli.upload(args.shift())
+    break;
 
-    default:
-      console.log(arg)
-  }
+  default:
+    console.log(arg)
 }
